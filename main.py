977ad3e31c8d4c729f2acd70fccc793bc939e6a8
Vandyck <vandycklai@gmail.com>
2024-06-12 15:33:19 +0800

    Add more data

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
 driver = webdriver.Firefox()
 
 url = 'https://app.virtuals.io/stake'
+# response = requests.get(url)
+# print(response.content)
 driver.get(url)
 
 html = driver.page_source
@@ -11,10 +14,10 @@ html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 images = soup.find_all('img', {"alt": "virtual character"})
 names = soup.find_all('p', class_="MuiTypography-root MuiTypography-body1 MuiTypography-noWrap css-yvh85y")
+virtuals = soup.find_all('p', class_="MuiTypography-root MuiTypography-body1 css-6d7ygw")
 
 
 with open('example_data.csv', 'w') as f:
-    f.write("Name, Image\n")
-    for virtual in zip(names, images):
-        print(virtual)
-        f.write("{}, {}\n".format(virtual[0].get_text(), virtual[1].get('src')))
+    f.write("Name, Image, Virtuals\n")
+    for virtual in zip(names, images, virtuals):
+        f.write("{}, {}, {}\n".format(virtual[0].get_text(), virtual[1].get('src'), virtual[2].get_text().replace(",", "")))
