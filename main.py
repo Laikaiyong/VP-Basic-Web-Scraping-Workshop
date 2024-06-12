2f8f49e2ebce7f32eb87e9620467d1a88552c66c
Vandyck <vandycklai@gmail.com>
2024-06-12 13:24:09 +0800

    Change to scrape virtuals.io

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
+driver = webdriver.Firefox()
 
-url = 'https://solscan.io/'
+url = 'https://app.virtuals.io/stake'
+driver.get(url)
 
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-links = soup.find_all('a')
+html = driver.page_source
 
-for link in links:
-    print(link.get('href'))
+soup = BeautifulSoup(html, 'html.parser')
+images = soup.find_all('img', {"alt": "virtual character"})
+names = soup.find_all('p', class_="MuiTypography-root MuiTypography-body1 MuiTypography-noWrap css-yvh85y")
+
 
 with open('example_data.csv', 'w') as f:
-    for link in links:
-        f.write(link.get('href'))
-        f.write("\n")
+    f.write("Name, Image\n")
+    for virtual in zip(names, images):
+        print(virtual)
+        f.write("{}, {}\n".format(virtual[0].get_text(), virtual[1].get('src')))
